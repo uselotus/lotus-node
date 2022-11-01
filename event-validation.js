7260db5ddf2061ed6d22685f0574c2f1e08ada48
Hardik Agarwal <hardikagarwal@192.168.1.7>
2022-11-02 00:51:44 +0530

    WIP

--- event-validation.js
+++ event-validation.js
@@ -1,9 +1,16 @@
-var type = require('component-type')
-var join = require('join-component')
-var assert = require('assert')
+const type = require('component-type')
+const join = require('join-component')
+const assert = require('assert')
+
+export const ValidateEventType = {
+    trackEvent : "trackEvent",
+    customerDetails : "customerDetails",
+    createCustomer : "createCustomer",
+    createSubscription : "createSubscription",
+}
 
 // Lotus messages can be a maximum of 32 kB.
-var MAX_SIZE = 32 << 10
+const MAX_SIZE = 32 << 10;
 
 module.exports = eventValidation
 
@@ -12,12 +19,18 @@ module.exports = eventValidation
  */
 
 function eventValidation(event, type) {
-    validateGenericEvent(event)
-    type = type || event.type
-    assert(type, 'You must pass an event type.')
     switch (type) {
-        case 'trackEvent':
+        case ValidateEventType.trackEvent:
+            validateGenericEvent(event)
+            type = type || event.type
+            assert(type, 'You must pass an event type.')
             return validateTrackEventEvent(event)
+        case ValidateEventType.customerDetails:
+            return validateCustomerDetailsEvent(event)
+        case ValidateEventType.createCustomer:
+            return validateCreateCustomerEvent(event)
+        case ValidateEventType.createSubscription:
+            return validateCreateSubscriptionEvent(event)
         default:
             assert(0, 'Invalid event type: "' + type + '"')
     }
@@ -28,9 +41,47 @@ function eventValidation(event, type) {
  */
 
 function validateTrackEventEvent(event) {
-    assert(event.idempotency_id, 'You must pass an "idempotency_id".')
-    assert(event.event_name, 'You must pass an "event_name".')
-    assert(event.customer_id, 'You must pass a "customer_id".')
+    if (!("event_name" in event || "eventName" in event)) {
+        throw new Error("event_name is a required key")
+    }
+
+    if (!("customer_id" in event || "customerId" in event)) {
+        throw new Error("customer_id is a required key")
+    }
+}
+
+/**
+ * Validate a "CustomerDetails" event.
+ */
+
+function validateCustomerDetailsEvent(event) {
+    if (!("customer_id" in event || "customerId" in event)) {
+        throw new Error("customer_id is a required key")
+    }
+}
+
+/**
+ * Validate a "CreateCustomer" event.
+ */
+
+function validateCreateCustomerEvent(event) {
+    if (!("customer_name" in event || "customerName" in event)) {
+        throw new Error("customer_name is a required key")
+    }
+}
+
+function validateCreateSubscriptionEvent(event) {
+    if (!("customer_id" in event || "customerId" in event)) {
+        throw new Error("customer_id is a required key")
+    }
+
+    if (!("plan_id" in event || "planId" in event)) {
+        throw new Error("plan_id is a required key")
+    }
+
+    if (!("start_date" in event || "startDate" in event)) {
+        throw new Error("start_date is a required key")
+    }
 }
 
 /**
@@ -51,7 +102,7 @@ var genericValidationRules = {
 
 function validateGenericEvent(event) {
     assert(type(event) === 'object', 'You must pass a message object.')
-    var json = JSON.stringify(event)
+    const json = JSON.stringify(event)
     // Strings are variable byte encoded, so json.length is not sufficient.
     assert(Buffer.byteLength(json, 'utf8') < MAX_SIZE, 'Your message must be < 32 kB.')
 
